c63dec9563011a6388d8b431ab347033b8a0e41d
kandy <matus.kanda.po@gmail.com>
2022-02-17 21:08:51 +0100

    feat: create new file for solution three

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from solution_one import execution_one
 from solution_two import execution_two
+from solution_three import execution_three
 
 
 def main():
     # execution_one()
-    execution_two()
+    # execution_two()
+    execution_three()
 
 
 if __name__ == "__main__":
---
+++ solution_three.py
@@ -0,0 +1,35 @@
+from collections import Counter
+from fei.ppds import Thread
+import threading
+
+
+lock = threading.Lock()
+
+
+class Shared:
+    def __init__(self, size):
+        self.counter = 0
+        self.end = size
+        self.elms = [0] * size
+
+
+# incrementing every index of given array using locks because of multi threads
+def do_count(shared):
+    while shared.counter != shared.end:
+        # this if statement is used to ensure not to access array out of his range
+        if shared.counter >= shared.end:
+            break
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
+
+
+# joined block of code for easier function call from main class in other file
+def execution_three():
+    shared = Shared(1_000_000)
+    t1 = Thread(do_count, shared)
+    t2 = Thread(do_count, shared)
+    t1.join()
+    t2.join()
+
+    counter = Counter(shared.elms)
+    print(counter.most_common())
