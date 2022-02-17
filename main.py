ffc30a5be95b934cb6a0ff5504862b6cff785f43
kandy <matus.kanda.po@gmail.com>
2022-02-17 15:32:27 +0100

    feat(incrementation): create basic multi thread incrementation

---
+++ main.py
@@ -0,0 +1,10 @@
+from solution_one import execution_one
+
+
+def main():
+    execution_one()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ solution_one.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from fei.ppds import Thread
+
+
+class Shared:
+    def __init__(self, size):
+        self.counter = 0
+        self.end = size
+        self.elms = [0] * size
+
+
+def do_count(shared):
+    while shared.counter != shared.end:
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
+
+
+def execution_one():
+    shared = Shared(1_000_000)
+    t1 = Thread(do_count, shared)
+    t2 = Thread(do_count, shared)
+    t1.join()
+    t2.join()
+
+    counter = Counter(shared.elms)
+    print(counter.most_common())
