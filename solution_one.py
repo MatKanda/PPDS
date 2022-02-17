a2d1ebe7b385d5922ec1bea1075bce17cec28ccb
kandy <matus.kanda.po@gmail.com>
2022-02-17 16:42:41 +0100

    fix: fix index out of range error while incrementing array values

--- solution_one.py
+++ solution_one.py
@@ -13,14 +13,19 @@ class Shared:
         self.elms = [0] * size
 
 
+# incrementing every index of given array using locks because of multi threads
 def do_count(shared):
     while shared.counter != shared.end:
+        # this if statement is used to ensure not to access array out of his range
+        if shared.counter == shared.end - 1:
+            break
         lock.acquire()
         shared.elms[shared.counter] += 1
         shared.counter += 1
         lock.release()
 
 
+# joined block of code for easier function call from main class in other file
 def execution_one():
     shared = Shared(1_000_000)
     t1 = Thread(do_count, shared)
