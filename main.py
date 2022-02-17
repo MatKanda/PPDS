e8a78f5ef2ad2834279aa19ad3e42196979bfda9
kandy <matus.kanda.po@gmail.com>
2022-02-17 20:30:32 +0100

    feat: create another file for multi thread incrementation for second solution

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from solution_one import execution_one
+from solution_two import execution_two
 
 
 def main():
-    execution_one()
+    # execution_one()
+    execution_two()
 
 
 if __name__ == "__main__":
