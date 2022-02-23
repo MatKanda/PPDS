e748530d91a4c1a203f911bef4a3d1abd3772d11
kandy <matus.kanda.po@gmail.com>
2022-02-23 15:06:50 +0100

    style: add docstrings to both reusable and simple barrier files

--- reusable_barrier.py
+++ reusable_barrier.py
@@ -3,13 +3,42 @@ from fei.ppds import print
 
 
 class Barrier:
+    """
+    Barrier class shared between threads.
+
+    Attributes
+    ----------
+    n -> number of threads
+    counter -> counter for number of threads that reached certain point of execution
+    mutex -> lock used in 'wait' method
+    event -> event used to send signals for threads to wait/don't wait
+
+    Return value
+    ------------
+    None
+    """
     def __init__(self, n):
+        """
+       Create attributes for Barrier class.
+
+       Parameters
+       ----------
+       n -> number of threads
+       """
         self.n = n
         self.counter = 0
         self.mutex = Mutex()
         self.event = Event()
 
     def wait(self):
+        """
+        Wait method of Barrier class used to count threads and increment/decrement counter
+        in purpose to release waiting threads.
+
+        Return value
+        ------------
+        None
+        """
         self.mutex.lock()
         self.counter += 1
         if self.counter == self.n:
@@ -19,18 +48,68 @@ class Barrier:
         self.event.wait()
 
     def clear(self):
+        """
+        Clear method of Barrier class used to reset event settings.
+
+        Return value
+        ------------
+        None
+        """
         self.event.clear()
 
 
 def rendezvous(thread_name):
+    """
+    Function used to simulate some program code before critical area for test purpose.
+
+    Parameters
+    ----------
+    thread_name: Name of thread currently using this function
+
+    Return value
+    ------------
+    None
+
+    :param thread_name: Name of thread currently using this function
+    """
     print('rendezvous: %s' % thread_name)
 
 
 def ko(thread_name):
+    """
+    Function used to simulate some program code of critical area for test purpose.
+
+    Parameters
+    ----------
+    thread_name: Name of thread currently using this function
+
+    Return value
+    ------------
+    None
+
+    :param thread_name: Name of thread currently using this function
+    """
     print('ko: %s' % thread_name)
 
 
 def barrier_example(barrier1, barrier2, thread_name):
+    """
+    Function simulating some program code to test barrier solution.
+
+    Parameters
+    ----------
+    barrier1 -> class of SimpleBarrier object\n
+    barrier2 -> id of the thread currently using this function\n
+    thread_name -> Name of thread currently using this function
+
+    Return value
+    ------------
+    None
+
+    :param barrier1: class of SimpleBarrier object number 1
+    :param barrier2: class of SimpleBarrier object number 2
+    :param thread_name: Name of thread currently using this function
+    """
     while True:
         barrier1.clear()
         rendezvous(thread_name)
@@ -41,6 +120,17 @@ def barrier_example(barrier1, barrier2, thread_name):
 
 
 def execute_code():
+    """
+    Function used to execute solution using functions and Classes
+
+    Parameters
+    ----------
+    None
+
+    Return value
+    ------------
+    None
+    """
     THREADS = 15
     threads = list()
     barrier1 = Barrier(THREADS)
--- simple_barrier.py
+++ simple_barrier.py
@@ -2,13 +2,43 @@ from fei.ppds import Thread, Semaphore, Mutex, print, Event
 
 
 class SimpleBarrier:
+    """
+    SimpleBarrier class shared between threads.
+
+    Attributes
+    ----------
+    n -> number of threads
+    counter -> counter for number of threads that reached certain point of execution
+    mutex -> lock used in 'wait' method
+    event -> event used to send signals for threads to wait/don't wait
+
+    Return value
+    ------------
+    None
+    """
+
     def __init__(self, n):
+        """
+        Create attributes for SimpleBarrier class.
+
+        Parameters
+        ----------
+        n -> number of threads
+        """
         self.n = n
         self.counter = 0
         self.mutex = Mutex()
         self.event = Event()
 
     def wait(self):
+        """
+        Wait method of SimpleBarrier class used to count threads and increment/decrement counter
+        in purpose to release waiting threads.
+
+        Return value
+        ------------
+        None
+        """
         self.mutex.lock()
         self.counter += 1
         if self.counter == self. n:
@@ -19,12 +49,38 @@ class SimpleBarrier:
 
 
 def barrier_example(barrier, thread_id):
+    """
+    Function simulating some program code to test barrier solution.
+
+    Parameters
+    ----------
+    barrier -> class of SimpleBarrier object\n
+    thread_id -> id of the thread currently using this function
+
+    Return value
+    ------------
+    None
+
+    :param barrier: class of SimpleBarrier object
+    :param thread_id: id of the thread currently using this function
+    """
     print(f"thread {thread_id} before barrier")
     barrier.wait()
     print(f"thread {thread_id} after barrier")
 
 
 def execute_code():
+    """
+    Function used to execute solution using functions and Classes
+
+    Parameters
+    ----------
+    None
+
+    Return value
+    ------------
+    None
+    """
     THREADS = 15
     barrier = SimpleBarrier(THREADS)
     threads = [Thread(barrier_example, barrier, i) for i in range(THREADS)]
