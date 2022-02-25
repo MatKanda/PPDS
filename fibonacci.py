f8670321c07773204ab8ee04b8f01e32e86d88e0
kandy <matus.kanda.po@gmail.com>
2022-02-25 20:35:59 +0100

    feat: add sync choices

--- fibonacci.py
+++ fibonacci.py
@@ -1,20 +1,20 @@
-from fei.ppds import Thread, Mutex, Event, Semaphore
+from fei.ppds import Thread, Mutex, Event, Semaphore, RandomSemaphore
 from fei.ppds import print
 from random import randint
 from time import sleep
+import time
 
 
 class Synchronization:
-    def __init__(self, n):
+    def __init__(self, n, synchronization_object):
         self.n = n
-        self.semaphore = Semaphore(0)
+        self.synchronization_object = synchronization_object
         self.counter = 0
         self.mutex = Mutex()
 
     def wait(self, thread_index):
         """
-        Wait method of Synchronization class used to count threads and increment/decrement counter
-        in purpose to release waiting threads.
+        Wait method of Synchronization class used to stop and let go different threads
 
         Return value
         ------------
@@ -22,9 +22,12 @@ class Synchronization:
         """
         self.mutex.lock()
         if thread_index == self.counter:
-            self.semaphore.signal(self.n)
+            if type(self.synchronization_object) is RandomSemaphore:
+                self.synchronization_object.signal(self.n)
+            if type(self.synchronization_object) is Event:
+                self.synchronization_object.set()
         self.mutex.unlock()
-        self.semaphore.wait()
+        self.synchronization_object.wait()
 
     def get_counter(self):
         return self.counter
@@ -46,14 +49,23 @@ def compute_fib(sync, i):
         compute_fib(sync, i)
 
 
-THREADS = 10
+# executable code
+start_time = time.time()
+
+THREADS = 15
 fib_seq = [0] * (THREADS + 2)
 fib_seq[1] = 1
 
-# sync_obj = Synchronization(THREADS, Semaphore(0))
-semaphore = Synchronization(THREADS)
+# example usage with Event
+sync_obj_event = Event()
 
+# example usage with Semaphore
+sync_obj_semaphore = Semaphore(0)
+
+semaphore = Synchronization(THREADS, sync_obj_semaphore)
 threads = [Thread(compute_fib, semaphore, i) for i in range(THREADS)]
 [t.join() for t in threads]
 
 print(fib_seq)
+
+print("--- %s seconds ---" % (time.time() - start_time))
