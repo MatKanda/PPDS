9fb452c24707a02b69b51e1b25df124f85844f69
kandy <matus.kanda.po@gmail.com>
2022-02-26 12:35:43 +0100

    style: add docstrings and comments

--- fibonacci.py
+++ fibonacci.py
@@ -6,6 +6,16 @@ import time
 
 
 class Synchronization:
+    """
+    Synchronization class shared between threads.
+
+    Attributes
+    ----------
+    n -> number of threads
+    counter -> counter for number of threads that reached certain point of execution
+    mutex -> lock used in 'wait' method
+    synchronization_object -> Object used to sync, Semaphore or Event
+    """
     def __init__(self, n, synchronization_object):
         self.n = n
         self.synchronization_object = synchronization_object
@@ -30,17 +40,52 @@ class Synchronization:
         self.synchronization_object.wait()
 
     def get_counter(self):
+        """
+        Method used to get current counter value.
+
+        Return value
+        ------------
+        counter
+        """
         return self.counter
 
     def increment_counter(self):
+        """
+        Method used to increment counter by 1
+
+        Return value
+        ------------
+        None
+        """
         self.counter += 1
 
 
 def compute_fib(sync, i):
+    """
+    Function to compute Fibonacci consequence.
+
+    Parameters
+    ----------
+    sync: Synchronization object\n
+    i: Current thread accessing the function
+
+    Return value
+    ------------
+    None
+
+    :param sync: Synchronization object
+    :param i: Current thread accessing the function
+    """
+
+    # exit if counter exceeded number of elements in sequence
     if sync.get_counter() >= THREADS:
         exit()
     sleep(randint(1, 10) / 10)
+
+    # wait for all threads
     sync.wait(i)
+
+    # compute if correct thread enters the function
     if i == sync.get_counter():
         fib_seq[i + 2] = fib_seq[i] + fib_seq[i + 1]
         sync.increment_counter()
@@ -62,8 +107,10 @@ sync_obj_event = Event()
 # example usage with Semaphore
 sync_obj_semaphore = Semaphore(0)
 
-semaphore = Synchronization(THREADS, sync_obj_semaphore)
-threads = [Thread(compute_fib, semaphore, i) for i in range(THREADS)]
+# create Synchronization object
+sync_obj = Synchronization(THREADS, sync_obj_semaphore)
+
+threads = [Thread(compute_fib, sync_obj, i) for i in range(THREADS)]
 [t.join() for t in threads]
 
 print(fib_seq)
