b8533649b2c5db9285d53a738ac8688929304683
kandy <matus.kanda.po@gmail.com>
2022-04-17 13:17:22 +0200

    style: add docstrings

--- async.py
+++ async.py
@@ -1,14 +1,38 @@
+"""
+Simple sync program used as base for next task where I will try to refactor it into the async code.
+
+Imports "time, asyncio" and "random".
+
+Author: Matúš Kanda
+"""
 import time
 from random import randint
 import asyncio
 
 
+"""
+Global variables.
+    CARDS: cards the players can play with
+    N: number of cards
+    TABLE: array representing table with 3 seats
+"""
 CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 N = 9
 TABLE = [0, 0, 0]
 
 
 async def player(name, seat):
+    """
+    Function representing player picking a card and putting it on the table.
+
+    Parameters
+    ----------
+    name: name of the player
+    seat: his seat at the table
+
+    :param name: name of the player
+    :param seat: his seat at the table
+    """
     global IS_CARDS
     print(f"{name} is putting a card on the table.")
     await asyncio.sleep(2)
@@ -17,6 +41,9 @@ async def player(name, seat):
 
 
 def result():
+    """
+    Function giving a winner of the game.
+    """
     winner = None
     if TABLE[0] > TABLE[1] and TABLE[0] > TABLE[2]:
         winner = 0
