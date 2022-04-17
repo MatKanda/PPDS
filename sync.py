4d068597753b94a9ac4cc215b1cf0a5d283adcb7
kandy <matus.kanda.po@gmail.com>
2022-04-17 12:39:06 +0200

    style: add docstrings

--- sync.py
+++ sync.py
@@ -1,14 +1,33 @@
+"""
+Simple sync program used as base for next task where I will try to refactor it into the async code.
+
+Imports "time" and "random".
+
+Author: Matúš Kanda
+"""
 import time
 from time import sleep
 from random import randint
 
 
+"""
+Global variables.
+    CARDS: cards the players can play with
+    N: number of cards
+    TABLE: array representing table with 3 seats
+"""
 CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 N = 9
 TABLE = [0, 0, 0]
 
 
 def player(name, seat):
+    """
+    Function representing player picking a card and putting it on the table.
+
+    :param name: name of the player
+    :param seat: his seat at the table
+    """
     print(f"{name} is putting a card on the table.")
     TABLE[seat] = CARDS[randint(0, N-1)]
     sleep(2)
@@ -16,6 +35,9 @@ def player(name, seat):
 
 
 def result():
+    """
+    Function giving a winner of the game.
+    """
     winner = None
     if TABLE[0] > TABLE[1] and TABLE[0] > TABLE[2]:
         winner = 0
